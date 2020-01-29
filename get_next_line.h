af9b2fde7e1d9383de95cc4f55331236dc3db847
Dofidog <58728833+Dofidog@users.noreply.github.com>
2020-01-29 14:34:32 +0200

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpaaso <tpaaso@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/12 11:30:23 by tpaaso            #+#    #+#             */
+/*   Updated: 2019/11/15 12:49:49 by tpaaso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# define MAX_FD 8192
+
+int		get_next_line(const int fd, char **line);
+
+#endif
